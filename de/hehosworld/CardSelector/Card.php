3a216e51ca058faec9df51a06fda6d10f5f90066
Heho <heho@hehosworld.de>
2011-10-26 00:00:21 +0200

    card implementation

--- de/hehosworld/CardSelector/Card.php
+++ de/hehosworld/CardSelector/Card.php
@@ -9,15 +9,69 @@ namespace de\hehosworld\CardSelector;
 class Card
 {
 	/**
+	 *
+	 * @var string
+	 */
+	private $name;
+	
+	/**
 	 * Types of the Card
 	 * 
 	 * @var array of String
 	 */
 	private $types;
 	
+	/**
+	 * Additional Information on Cards
+	 * 
+	 * @var array of array;
+	 */
+	private $information;
 	
-	function __construct() 
+	/**
+	 *
+	 * @param string $name
+	 * @param array of string $types
+	 * @param array of array $information 
+	 */
+	function __construct($name, $types, $information = array()) 
 	{
-		
+		$this->name = $name;
+		$this->types = $types;
+		$this->information = $information;
+	}
+	
+	/**
+	 * @param String $type
+	 * @return Boolean 
+	 */
+	function hasType($type)
+	{
+		return in_array($type, $this->types);
+	}
+	
+	/**
+	 * @return array of String
+	 */
+	function getTypes()
+	{
+		return $this->types;
+	}
+	
+	/**
+	 *
+	 * @return String
+	 */
+	function getName()
+	{
+		return $this->name;
+	}
+	
+	/**
+	 * @return array of arrays
+	 */
+	function getInformation()
+	{
+		return $this->information;
 	}
 }
