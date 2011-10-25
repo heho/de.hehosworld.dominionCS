582e8f4310e4ee84b7505617750cadc12969edbd
Heho <heho@hehosworld.de>
2011-10-25 22:01:43 +0200

    Classes added

---
+++ de/hehosworld/CardSelector/Card.php
@@ -0,0 +1,23 @@
+<?php
+namespace de\hehosworld\CardSelector;
+
+/**
+ * Description of Card
+ *
+ * @author heho
+ */
+class Card
+{
+	/**
+	 * Types of the Card
+	 * 
+	 * @var array of String
+	 */
+	private $types;
+	
+	
+	function __construct() 
+	{
+		
+	}
+}
---
+++ de/hehosworld/CardSelector/CardSelector.php
@@ -0,0 +1,34 @@
+<?php
+namespace de\hehosworld\CardSelector;
+
+/**
+ * Description of CardSelector
+ *
+ * @author heho
+ */
+class CardSelector
+{
+	/**
+	 * Number of Cards to be selected
+	 * 
+	 * @var integer
+	 */
+	private $cardCount;
+
+	/**
+	 * 
+	 * @var array of
+	 */
+	private $cards;
+	
+	
+	function __construct($configSource)
+	{
+		
+	}
+	
+	function generateCardArray()
+	{
+		
+	}
+}
